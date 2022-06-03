491520f4b091f5fab23b578fd62f6f078662cf43
Taylor Roberts <tstarrroberts@gmail.com>
2022-06-03 02:16:14 -0400

    add seeds

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,25 @@ CREATE DATABASE employees;
 
 USE employees;
 
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS employee;
-
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INTEGER,
+    department_id INTEGER NULL,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
+    role_id INTEGER NULL,
+    manager_id INTEGER NULL,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON UPDATE CASCADE ON DELETE SET NULL,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON UPDATE CASCADE ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES
+    ('Finance'),
+    ('Marketing'),
+    ('Human Resources'),
+    ('IT');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('CPA', 90000, 1),
+    ('Finance Officer', 140000, 1),
+    ('Junior Marketing Associate', 55000, 2),
+    ('Marketing Manager', 100000, 2),
+    ('HR Associate', 85000, 3),
+    ('HR Manager', 120000, 3),
+    ('Software Engineer', 88000, 4),
+    ('IT Manager', 140000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Wayne', 1, 2),
+    ('Michael', 'Scott', 2, null),
+    ('Matthew', 'Bargamin', 3, 4),
+    ('Marcus', 'Lang', 4, null),
+    ('Jonathan', 'Smith', 5, 6),
+    ('Dwight', 'Schrute', 6, null),
+    ('Taylor', 'Roberts', 7, 8),
+    ('Charles', 'Darwin', 8, null);
