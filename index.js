c342160f5e261f1ca6cbc7778f883e98a35b645d
Taylor Roberts <tstarrroberts@gmail.com>
2022-06-04 11:54:23 -0400

    add view employees

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const promptUser = () => {
                 break;
 
             case 'View all employees':
-                // function here
+                viewEmployees();
                 break;
 
             case 'Add a department':
@@ -87,6 +87,26 @@ const viewRoles = () => {
               console.table(res);
               // return to role section
               promptUser();
+    });
+};
+
+const viewEmployees = () => {
+    db.query(`SELECT CONCAT (employee.first_name, " ", employee.last_name) AS Name, 
+            role.title AS Title, 
+            department.name AS Department,
+            role.salary as Salary, 
+            CONCAT (manager.first_name, " ", manager.last_name) AS Manager
+            FROM employee
+            LEFT JOIN role ON employee.role_id = role.id
+            LEFT JOIN department ON role.department_id = department.id
+            LEFT JOIN employee manager ON employee.manager_id = manager.id
+            `,
+            (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                // return to role section
+                promptUser();
+              
     });
 };
 
